f0f4f39c84942861a685b64db386ace098c64f76
psoto-go <pabletesglez@gmail.com>
2021-06-02 20:49:40 +0000

    falta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const simbolos = ["♦", "♥", "♠", "♣"];
+
+function Randomcard(simbolos) {
+  return simbolos[Math.floor(Math.random() * simbolos.length)];
+}
+
+function Randomnum(simbolos) {
+  var sim = Math.floor(Math.random() * 12) + 2;
+  if (sim === 11) {
+    sim = "J";
+  } else if (sim === 12) {
+    sim = "Q";
+  } else if (sim === 13) {
+    sim = "K";
+  }
+  return sim;
+}
+
+window.onload = function refresh() {
+  document.querySelector(".numero").innerHTML = Randomnum();
+  let array1 = document.querySelectorAll(".simbolo");
+  array1.forEach(element => (element.innerHTML = Randomcard(simbolos)));
 };
+
+document.getElementById("button").addEventListener("click", function() {
+  document.querySelector(".numero").innerHTML = Randomnum();
+  let array1 = document.querySelectorAll(".simbolo");
+  array1.forEach(element => (element.innerHTML = Randomcard(simbolos)));
+});
